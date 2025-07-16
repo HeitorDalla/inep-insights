e644a335e8991f97ce7f26f23a5c94ed2fdbca80
HeitorDalla <heitorvillavilla@gmail.com>
2025-07-16 10:48:52 -0300

    Mudado os filtros principais da pagina de home, colocando a opcao de todos, caso o usuário nao queira colocar filtro nenhum

--- frontend/views/home.py
+++ frontend/views/home.py
@@ -3,44 +3,78 @@ import pandas as pd
 
 # Função para mostrar a página home
 def show_home_page (conn):
+    # Regiões
+
     # SQL Query p/ ler as regiões
     regiao_unique = pd.read_sql("""
         SELECT DISTINCT NO_REGIAO
         FROM regiao
         ORDER BY NO_REGIAO ASC
     """, conn)
-    
-    # Selectbox das regiões
-    regiao_selecionada = st.sidebar.selectbox("Selecione a região:", options=regiao_unique)
+
+    # Inserindo a opção todos no início
+    regiao_options = ['Todos'] + regiao_unique['NO_REGIAO'].tolist()
+    regiao_selecionada = st.sidebar.selectbox("Selecione a região:", options=regiao_options)
+
+    # UFs
 
     # SQL Query p/ ler as UFs
-    uf_unique = pd.read_sql("""
-        SELECT DISTINCT uf.NO_UF
-        FROM uf
-        JOIN regiao
-            ON uf.regiao_id = regiao.id
-        WHERE regiao.NO_REGIAO = %s
-        ORDER BY uf.NO_UF ASC
-    """, conn, params=(regiao_selecionada,))
+    if regiao_selecionada == 'Todos':
+        # Pega todas as UFs
+        uf_unique = pd.read_sql("""
+            SELECT DISTINCT NO_UF
+            FROM uf
+            ORDER BY NO_UF ASC
+        """, conn)
+    else:
+        # Pegar as UFs que faz parte da região escolhida
+        uf_unique = pd.read_sql("""
+            SELECT DISTINCT uf.NO_UF
+            FROM uf
+            JOIN regiao
+                ON uf.regiao_id = regiao.id
+            WHERE regiao.NO_REGIAO = %s
+            ORDER BY uf.NO_UF ASC
+        """, conn, params=(regiao_selecionada,))
 
-    # Selectbox das UFs
-    uf_selecionada = st.sidebar.selectbox("Selecione a UF:", options=uf_unique)
+    # Inserindo a opção 'Todas' nas UFs
+    uf_options = ['Todos'] + uf_unique['NO_UF'].tolist()
+    uf_selecionada = st.sidebar.selectbox("Selecione a UF:", options=uf_options)
+
+    # Municípios
 
     # SQL Query p/ ler os municípios
-    municipio_unique = pd.read_sql_query("""
-        SELECT DISTINCT m.NO_MUNICIPIO
-        FROM municipio AS m
-        JOIN uf AS u
-            ON m.uf_id = u.id
-        WHERE u.NO_UF = %s
-        ORDER BY m.NO_MUNICIPIO ASC
-    """, conn, params=(uf_selecionada,))
+    if uf_selecionada == 'Todos':
+        if regiao_selecionada == 'Todos':
+            municipio_unique = pd.read_sql_query("""
+                SELECT DISTINCT NO_MUNICIPIO
+                FROM municipio
+                ORDER BY NO_MUNICIPIO ASC
+            """, conn)
+        else:
+            municipio_unique = pd.read_sql_query("""
+                SELECT DISTINCT m.NO_MUNICIPIO
+                FROM municipio AS m
+                JOIN uf AS u ON m.uf_id = u.id
+                JOIN regiao AS r ON u.regiao_id = r.id
+                WHERE r.NO_REGIAO = %s
+                ORDER BY m.NO_MUNICIPIO ASC
+            """, conn, params=(regiao_selecionada,))
+    else:
+        municipio_unique = pd.read_sql_query("""
+                SELECT DISTINCT m.NO_MUNICIPIO
+                FROM municipio AS m
+                JOIN uf AS u
+                    ON m.uf_id = u.id
+                WHERE u.NO_UF = %s
+                ORDER BY m.NO_MUNICIPIO ASC
+            """, conn, params=(uf_selecionada,))
 
-    # Selectbox dos municípios
-    municipio_selecionado = st.sidebar.selectbox("Selecione o município:", municipio_unique)
+    # Adicionando a opção 'todos' nos municípios
+    municipios_options = ['Todos'] + municipio_unique['NO_MUNICIPIO'].tolist()
+    municipio_selecionado = st.sidebar.selectbox("Selecione o município:", municipios_options)
 
     # Configuração dos KPIs
-
     col1, col2, col3 = st.columns(3)
 
     with col1:
